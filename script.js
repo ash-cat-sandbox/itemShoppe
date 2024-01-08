9079e1d7f8e128c6876e0e73ae3d7cba0577ef74
Ash Pedicini <ash.cat.sandbox@gmail.com>
2024-01-08 06:07:55 -0800

    Added script variables

---
+++ script.js
@@ -0,0 +1,19 @@
+let charisma = 0;
+let health = 100;
+let gold = 50;
+let inventory = [];
+let weapons = [];
+let currentWeapon = 0;
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const button4 = document.querySelector('#button4');
+const text    = document.querySelector('#text');
+const charismaText = document.querySelector('#charismaText');
+const healthText   = document.querySelector('#healthText');
+const goldText = document.querySelector('#goldText');
+const monsterStats = document.querySelector('#monsterStats');
+const monsterName = document.querySelector('#monsterName');
+const monsterHealthText = document.querySelector('#monsterHealth');
+
